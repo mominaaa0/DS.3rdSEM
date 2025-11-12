1f1a778c33eb7d3ef6ec2758ddf243defccb1a0a
mominaaa0 <mominafatima179@gmail.com>
2025-11-12 11:34:04 +0530

    Update bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
-
 int main() {
     int arr[10], n, i, j, temp;
-
     printf("Enter number of elements: ");
     scanf("%d", &n);
-
     printf("Enter %d elements:\n", n);
     for(i = 0; i < n; i++)
         scanf("%d", &arr[i]);
-
-    // Bubble Sort
     for(i = 0; i < n-1; i++) {
         for(j = 0; j < n-1-i; j++) {
-            if(arr[j] > arr[j+1]) {   // swap if next is smaller
+            if(arr[j] > arr[j+1]) {
                 temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
             }
         }
     }
-
     printf("Array after sorting in ascending order: ");
     for(i = 0; i < n; i++)
         printf("%d ", arr[i]);
-
     return 0;
 }
