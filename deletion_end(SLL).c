a6c96cde2316e74e7ff7661846f6a68b3903bea4
mominaaa0 <mominafatima179@gmail.com>
2025-11-12 11:41:00 +0530

    Update deletion_end(SLL).c

---
+++ deletion_end(SLL).c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* next;
+};
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+struct Node* createLinkedList(int n) {
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+    int data, i;
+    if (n <= 0) {
+        printf("Number of nodes should be greater than zero.\n");
+        return NULL;
+    }
+    printf("Enter data for node 1: ");
+    scanf("%d", &data);
+    head = createNode(data);
+    temp = head;
+    for (i = 2; i <= n; i++) {
+        printf("Enter data for node %d: ", i);
+        scanf("%d", &data);
+        newNode = createNode(data);
+        temp->next = newNode;
+        temp = temp->next;
+    }
+    return head;
+}
+void displayLinkedList(struct Node* head) {
+    struct Node* temp = head;
+    if (head == NULL) {
+        printf("List is empty.\n");
+        return;
+    }
+    printf("Linked List: ");
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+struct Node* deleteFromEnd(struct Node* head) {
+    if (head == NULL) {
+        printf("\nList is empty. Nothing to delete.\n");
+        return NULL;
+    }
+    if (head->next == NULL) {
+        printf("\nDeleted node: %d\n", head->data);
+        free(head);
+        return NULL;
+    }
+    struct Node *temp = head, *prev = NULL;
+    while (temp->next != NULL) {
+        prev = temp;
+        temp = temp->next;
+    }
+    printf("\nDeleted node: %d\n", temp->data);
+    free(temp);
+    prev->next = NULL;
+    return head;
+}
+int main() {
+    struct Node* head = NULL;
+    int n;
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+    head = createLinkedList(n);
+    printf("\nBefore deletion:\n");
+    displayLinkedList(head);
+    head = deleteFromEnd(head);
+    printf("\nAfter deletion:\n");
+    displayLinkedList(head);
+    return 0;
+}
